a2f59d2c0158745c5c15cd46f2ee915a627f44d3
Rizwan Ali <rizalishan@gmial.com>
2018-10-02 02:32:41 -0700

    Ini

---
+++ Assets/Scripts/MQTTManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+using System;
+
+public class MQTTManager : MonoBehaviour {
+
+    private MqttClient Client;
+
+    public string SubscriberURL;
+    // Use this for initialization
+    void Start () {
+        Client = new MqttClient("iot.eclipse.org");
+        Client.Connect(Guid.NewGuid().ToString());
+
+        Client.MqttMsgPublishReceived += MessageArrivedCallback;
+        Client.Subscribe(new string[] { SubscriberURL }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+    }
+
+    private void MessageArrivedCallback(object sender, MqttMsgPublishEventArgs e)
+    {
+        if (e.Topic == "/i5/hololens/instructions")
+        {
+            UnityMainThreadDispatcher.Instance().Enqueue(() =>
+            {
+                TextMesh t = (TextMesh)GetComponent(typeof(TextMesh));
+                t.text = System.Text.Encoding.UTF8.GetString(e.Message);
+            });
+        }
+        else if (e.Topic == "/i5/hololens/car/ldoor")
+        {
+            UnityMainThreadDispatcher.Instance().Enqueue(() =>
+            {
+
+                OpenElement t = (OpenElement)GetComponent(typeof(OpenElement));
+                if (System.Text.Encoding.UTF8.GetString(e.Message) == "open")
+                {
+                    t.Opening.SetInteger("EtatAnim", 1);
+                }
+                else
+                {
+                    t.Opening.SetInteger("EtatAnim", 2);
+                }
+            });
+        }
+        else if (e.Topic == "/i5/hololens/car/rdoor")
+        {
+            UnityMainThreadDispatcher.Instance().Enqueue(() =>
+            {
+
+                OpenElement t = (OpenElement)GetComponent(typeof(OpenElement));
+                if (System.Text.Encoding.UTF8.GetString(e.Message) == "open")
+                {
+                    t.Opening.SetInteger("EtatAnim", 1);
+                }
+                else
+                {
+                    t.Opening.SetInteger("EtatAnim", 2);
+                }
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour {
+
+    private GameObject Instructions;
+
+	// Use this for initialization
+	void Start () {
+        if (Instructions == null) Instructions = GameObject.Find("Instructions");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Instructions == null) return;
+        Instructions.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
+        Instructions.transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
+        Instructions.transform.localScale = Vector3.one * 0.0075f;
+	}
+}
